b1ef76ceb9f91139063e767e514eb74a119ebf4a
Sefa Bulak <zckhtln@icloud.com>
2025-04-11 09:59:32 +0300

    Update LastNewsTwo.vue

--- components/LastNewsTwo.vue
+++ components/LastNewsTwo.vue
@@ -1,131 +1,105 @@
 <template>
   <div class="col-4">
     <div class="slider-s-2" id="sliders2">
-      <div class="swiper-container swiper-container-horizontal">
+      <div class="swiper-container">
         <div class="swiper-wrapper" style="transform: translate3d(-1590px, 0px, 0px); transition-duration: 0ms">
+          <!-- Slide 1 (Duplicate) -->
           <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="4" style="width: 318px">
             <div class="image-wrapper image-330">
-              <a target="_blank" href="kobi/index.html">
-                <img class="image lazy" alt="Halkbank" src="https://assets.turkmedya.com.tr/halkbank-2025-subat-2/318x330.jpg" style="" />
+              <a target="_blank" href="/kobi/index.html">
+                <img class="image lazy" alt="Halkbank" src="https://assets.turkmedya.com.tr/halkbank-2025-subat-2/318x330.jpg" />
               </a>
             </div>
           </div>
 
+          <!-- Slide 2 -->
           <div class="swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="0" style="width: 318px">
-            <a target="_blank" title="Her gÃ¯Â¿Â½n 1 fincan kahve iÃ¯Â¿Â½menin 10 faydasÃ¯Â¿Â½" href="mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">
+            <a target="_blank" title="Her gün 1 fincan kahve içmenin 10 faydası" href="/mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">
               <div class="image-wrapper image-330">
-                <img
-                  class="image"
-                  src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg"
-                  srcset="
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg 1x,
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg 2x
-                  "
-                  alt="Her gÃ¯Â¿Â½n 1 fincan kahve iÃ¯Â¿Â½menin 10 faydasÃ¯Â¿Â½" />
+                <img class="image" src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg" alt="Her gün 1 fincan kahve içmenin 10 faydası" />
               </div>
             </a>
           </div>
 
+          <!-- Slide 3 -->
           <div class="swiper-slide" data-swiper-slide-index="1" style="width: 318px">
             <a
               target="_blank"
-              title="Mehmet Ali Erbil'in isteÃ¯Â¿Â½i yok artÃ¯Â¿Â½k dedirtti!"
-              href="venus/uc-cocuk-bir-torun-sahibi-mehmet-ali-erbilin-istegi-yok-artik-dedirtti/haber-1560479.html">
+              title="Mehmet Ali Erbil'in isteği yok artık dedirtti!"
+              href="/venus/uc-cocuk-bir-torun-sahibi-mehmet-ali-erbilin-istegi-yok-artik-dedirtti/haber-1560479.html">
               <div class="image-wrapper image-330">
                 <img
                   class="image lazy"
-                  srcset="
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/uc-cocuk-bir-torun-sahibi-384.jpg 1x,
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/uc-cocuk-bir-torun-sahibi-384.jpg 2x
-                  "
-                  alt="Mehmet Ali Erbil'in isteÃ¯Â¿Â½i yok artÃ¯Â¿Â½k dedirtti!"
                   src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/uc-cocuk-bir-torun-sahibi-384.jpg"
-                  style="" />
+                  alt="Mehmet Ali Erbil'in isteği yok artık dedirtti!" />
               </div>
             </a>
           </div>
 
+          <!-- Slide 4 -->
           <div class="swiper-slide" data-swiper-slide-index="2" style="width: 318px">
             <a
               target="_blank"
-              title="KiÃ¯Â¿Â½ilik testi: Resimde ilk gÃ¯Â¿Â½rdÃ¯Â¿Â½Ã¯Â¿Â½Ã¯Â¿Â½n yÃ¯Â¿Â½z hangisi? Cimri misin yoksaÃ¯Â¿Â½"
-              href="mor-papatya/kisilik-testi-resimde-ilk-gordugun-yuz-hangisi-cimri-misin-yoksa/haber-1560464.html">
+              title="Kişilik testi: Resimde ilk gördüğün yüz hangisi? Cimri misin yoksa"
+              href="/mor-papatya/kisilik-testi-resimde-ilk-gordugun-yuz-hangisi-cimri-misin-yoksa/haber-1560464.html">
               <div class="image-wrapper image-330">
                 <img
                   class="image lazy"
-                  srcset="
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/kisilik-testi-resimde-ilk-587.jpg 1x,
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/kisilik-testi-resimde-ilk-587.jpg 2x
-                  "
-                  alt="KiÃ¯Â¿Â½ilik testi: Resimde ilk gÃ¯Â¿Â½rdÃ¯Â¿Â½Ã¯Â¿Â½Ã¯Â¿Â½n yÃ¯Â¿Â½z hangisi? Cimri misin yoksaÃ¯Â¿Â½"
                   src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/kisilik-testi-resimde-ilk-587.jpg"
-                  style="" />
+                  alt="Kişilik testi: Resimde ilk gördüğün yüz hangisi? Cimri misin yoksa" />
               </div>
             </a>
           </div>
 
+          <!-- Slide 5 -->
           <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="3" style="width: 318px">
             <a
               target="_blank"
-              title="Ã¯Â¿Â½brahim TatlÃ¯Â¿Â½ses'ten Dilan'Ã¯Â¿Â½ kÃ¯Â¿Â½skandÃ¯Â¿Â½racak paylaÃ¯Â¿Â½Ã¯Â¿Â½m"
-              href="venus/ibrahim-tatlisesten-kizi-dilan-citaki-kiskandiracak-paylasim/haber-1560460.html">
+              title="İbrahim Tatlıses'ten Dilan'ı kıskandıracak paylaşım"
+              href="/venus/ibrahim-tatlisesten-kizi-dilan-citaki-kiskandiracak-paylasim/haber-1560460.html">
               <div class="image-wrapper image-330">
                 <img
                   class="image lazy"
-                  srcset="
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/ibrahim-tatlisesten-kizi--164.jpg 1x,
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/ibrahim-tatlisesten-kizi--164.jpg 2x
-                  "
-                  alt="Ã¯Â¿Â½brahim TatlÃ¯Â¿Â½ses'ten Dilan'Ã¯Â¿Â½ kÃ¯Â¿Â½skandÃ¯Â¿Â½racak paylaÃ¯Â¿Â½Ã¯Â¿Â½m"
                   src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/ibrahim-tatlisesten-kizi--164.jpg"
-                  style="" />
+                  alt="İbrahim Tatlıses'ten Dilan'ı kıskandıracak paylaşım" />
               </div>
             </a>
           </div>
 
+          <!-- Slide 6 (Active) -->
           <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="4" style="width: 318px">
             <div class="image-wrapper image-330">
-              <a target="_blank" href="kobi/index.html">
-                <img class="image lazy" alt="Halkbank" src="https://assets.turkmedya.com.tr/halkbank-2025-subat-2/318x330.jpg" style="" />
+              <a target="_blank" href="/kobi/index.html">
+                <img class="image lazy" alt="Halkbank" src="https://assets.turkmedya.com.tr/halkbank-2025-subat-2/318x330.jpg" />
               </a>
             </div>
           </div>
 
+          <!-- Slide 7 (Duplicate) -->
           <div class="swiper-slide swiper-slide-duplicate swiper-slide-next" data-swiper-slide-index="0" style="width: 318px">
-            <a target="_blank" title="Her gÃ¯Â¿Â½n 1 fincan kahve iÃ¯Â¿Â½menin 10 faydasÃ¯Â¿Â½" href="mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">
+            <a target="_blank" title="Her gün 1 fincan kahve içmenin 10 faydası" href="/mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">
               <div class="image-wrapper image-330">
-                <img
-                  class="image"
-                  src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg"
-                  srcset="
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg 1x,
-                    https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg 2x
-                  "
-                  alt="Her gÃ¯Â¿Â½n 1 fincan kahve iÃ¯Â¿Â½menin 10 faydasÃ¯Â¿Â½" />
+                <img class="image" src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg" alt="Her gün 1 fincan kahve içmenin 10 faydası" />
               </div>
             </a>
           </div>
         </div>
       </div>
       <ul class="s-pagination flex">
-        <li class="">
-          <a target="_blank" data-slide-index="0" href="mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">1</a>
+        <li>
+          <a target="_blank" data-slide-index="0" href="/mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">1</a>
         </li>
-
-        <li class="">
-          <a target="_blank" data-slide-index="1" href="venus/uc-cocuk-bir-torun-sahibi-mehmet-ali-erbilin-istegi-yok-artik-dedirtti/haber-1560479.html">2</a>
+        <li>
+          <a target="_blank" data-slide-index="1" href="/venus/uc-cocuk-bir-torun-sahibi-mehmet-ali-erbilin-istegi-yok-artik-dedirtti/haber-1560479.html">2</a>
         </li>
-
-        <li class="">
-          <a target="_blank" data-slide-index="2" href="mor-papatya/kisilik-testi-resimde-ilk-gordugun-yuz-hangisi-cimri-misin-yoksa/haber-1560464.html">3</a>
+        <li>
+          <a target="_blank" data-slide-index="2" href="/mor-papatya/kisilik-testi-resimde-ilk-gordugun-yuz-hangisi-cimri-misin-yoksa/haber-1560464.html">3</a>
         </li>
-
-        <li class="">
-          <a target="_blank" data-slide-index="3" href="venus/ibrahim-tatlisesten-kizi-dilan-citaki-kiskandiracak-paylasim/haber-1560460.html">4</a>
+        <li>
+          <a target="_blank" data-slide-index="3" href="/venus/ibrahim-tatlisesten-kizi-dilan-citaki-kiskandiracak-paylasim/haber-1560460.html">4</a>
         </li>
-
         <li class="active">
-          <a target="_blank" data-slide-index="1" href="kobi/index.html">5</a>
+          <a target="_blank" data-slide-index="4" href="/kobi/index.html">5</a>
         </li>
       </ul>
       <i class="icon icon-nav font-size-18 icon-left-arrow-line cursor-pointer"></i>
