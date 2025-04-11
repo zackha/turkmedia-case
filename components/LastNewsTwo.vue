378575c75f2faa440cb733db6d68cb7752091a59
Sefa Bulak <zckhtln@icloud.com>
2025-04-11 10:14:27 +0300

    Refactor LastNewsTwo component: simplify slide structure and remove unnecessary href attributes for improved clarity and performance.

--- components/LastNewsTwo.vue
+++ components/LastNewsTwo.vue
@@ -2,31 +2,17 @@
   <div class="col-4">
     <div class="slider-s-2" id="sliders2">
       <div class="swiper-container">
-        <div class="swiper-wrapper" style="transform: translate3d(-1590px, 0px, 0px); transition-duration: 0ms">
-          <!-- Slide 1 (Duplicate) -->
-          <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="4" style="width: 318px">
-            <div class="image-wrapper image-330">
-              <a target="_blank" href="/kobi/index.html">
-                <img class="image lazy" alt="Halkbank" src="https://assets.turkmedya.com.tr/halkbank-2025-subat-2/318x330.jpg" />
-              </a>
-            </div>
-          </div>
-
-          <!-- Slide 2 -->
+        <div class="swiper-wrapper">
           <div class="swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="0" style="width: 318px">
-            <a target="_blank" title="Her gün 1 fincan kahve içmenin 10 faydası" href="/mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">
+            <a title="Her gün 1 fincan kahve içmenin 10 faydası">
               <div class="image-wrapper image-330">
                 <img class="image" src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg" alt="Her gün 1 fincan kahve içmenin 10 faydası" />
               </div>
             </a>
           </div>
 
-          <!-- Slide 3 -->
           <div class="swiper-slide" data-swiper-slide-index="1" style="width: 318px">
-            <a
-              target="_blank"
-              title="Mehmet Ali Erbil'in isteği yok artık dedirtti!"
-              href="/venus/uc-cocuk-bir-torun-sahibi-mehmet-ali-erbilin-istegi-yok-artik-dedirtti/haber-1560479.html">
+            <a target="_blank" title="Mehmet Ali Erbil'in isteği yok artık dedirtti!">
               <div class="image-wrapper image-330">
                 <img
                   class="image lazy"
@@ -36,12 +22,8 @@
             </a>
           </div>
 
-          <!-- Slide 4 -->
           <div class="swiper-slide" data-swiper-slide-index="2" style="width: 318px">
-            <a
-              target="_blank"
-              title="Kişilik testi: Resimde ilk gördüğün yüz hangisi? Cimri misin yoksa"
-              href="/mor-papatya/kisilik-testi-resimde-ilk-gordugun-yuz-hangisi-cimri-misin-yoksa/haber-1560464.html">
+            <a target="_blank" title="Kişilik testi: Resimde ilk gördüğün yüz hangisi? Cimri misin yoksa">
               <div class="image-wrapper image-330">
                 <img
                   class="image lazy"
@@ -51,12 +33,8 @@
             </a>
           </div>
 
-          <!-- Slide 5 -->
           <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="3" style="width: 318px">
-            <a
-              target="_blank"
-              title="İbrahim Tatlıses'ten Dilan'ı kıskandıracak paylaşım"
-              href="/venus/ibrahim-tatlisesten-kizi-dilan-citaki-kiskandiracak-paylasim/haber-1560460.html">
+            <a target="_blank" title="İbrahim Tatlıses'ten Dilan'ı kıskandıracak paylaşım">
               <div class="image-wrapper image-330">
                 <img
                   class="image lazy"
@@ -66,18 +44,16 @@
             </a>
           </div>
 
-          <!-- Slide 6 (Active) -->
           <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="4" style="width: 318px">
             <div class="image-wrapper image-330">
-              <a target="_blank" href="/kobi/index.html">
+              <a>
                 <img class="image lazy" alt="Halkbank" src="https://assets.turkmedya.com.tr/halkbank-2025-subat-2/318x330.jpg" />
               </a>
             </div>
           </div>
 
-          <!-- Slide 7 (Duplicate) -->
           <div class="swiper-slide swiper-slide-duplicate swiper-slide-next" data-swiper-slide-index="0" style="width: 318px">
-            <a target="_blank" title="Her gün 1 fincan kahve içmenin 10 faydası" href="/mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">
+            <a title="Her gün 1 fincan kahve içmenin 10 faydası">
               <div class="image-wrapper image-330">
                 <img class="image" src="https://img3.aksam.com.tr/imgsdisk/2025/04/09/her-gun-1-fincan-kahve-ic-925.jpg" alt="Her gün 1 fincan kahve içmenin 10 faydası" />
               </div>
@@ -87,19 +63,19 @@
       </div>
       <ul class="s-pagination flex">
         <li>
-          <a target="_blank" data-slide-index="0" href="/mor-papatya/her-gun-1-fincan-kahve-icmenin-10-faydasi/haber-1560480.html">1</a>
+          <a data-slide-index="0" href="">1</a>
         </li>
         <li>
-          <a target="_blank" data-slide-index="1" href="/venus/uc-cocuk-bir-torun-sahibi-mehmet-ali-erbilin-istegi-yok-artik-dedirtti/haber-1560479.html">2</a>
+          <a data-slide-index="1" href="">2</a>
         </li>
         <li>
-          <a target="_blank" data-slide-index="2" href="/mor-papatya/kisilik-testi-resimde-ilk-gordugun-yuz-hangisi-cimri-misin-yoksa/haber-1560464.html">3</a>
+          <a data-slide-index="2" href="">3</a>
         </li>
         <li>
-          <a target="_blank" data-slide-index="3" href="/venus/ibrahim-tatlisesten-kizi-dilan-citaki-kiskandiracak-paylasim/haber-1560460.html">4</a>
+          <a data-slide-index="3" href="">4</a>
         </li>
         <li class="active">
-          <a target="_blank" data-slide-index="4" href="/kobi/index.html">5</a>
+          <a data-slide-index="4" href="">5</a>
         </li>
       </ul>
       <i class="icon icon-nav font-size-18 icon-left-arrow-line cursor-pointer"></i>
